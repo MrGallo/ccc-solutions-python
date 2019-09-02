42463478acd92a6263c8fae3a26e98d2d4da7fa0
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-09-02 08:52:23 -0400

    Rename loop variable

--- 2000 Junior/j2_9966.py
+++ 2000 Junior/j2_9966.py
@@ -6,12 +6,12 @@ flippable = 0
 while n <= end:
     split_n = list(str(n))
     replaced_n = []
-    for num in split_n:
-        if num == "0" or num == "1" or num == "8":
-            replaced_n.append(num)
-        elif num == "6":
+    for char in split_n:
+        if char == "0" or char == "1" or char == "8":
+            replaced_n.append(char)
+        elif char == "6":
             replaced_n.append("9")
-        elif num == "9":
+        elif char == "9":
             replaced_n.append("6")
         else:
             replaced_n.append(None)
